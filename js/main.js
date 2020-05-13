6fe05d28f34eabbf4ab0d53b0410b95df98827a6
wookiee2187 <nlingareddy@uchicago.edu>
2020-05-13 13:27:40 -0400

    more interactive changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(innerWidth, innerHeight)
-  background(0,0,0,100)
+  background(255,255,255,100)
   let x = width / 2
   let y = height / 2
   let r = width / 4
@@ -15,13 +15,19 @@ function mouseDragged() {
     square(pmouseX, pmouseY, mouseX - mouseY);
     //circle(pmouseX, pmouseY, mouseX - mouseY);
     fill(c);
-    emissiveMaterial(130, 230, 0);
+    //emissiveMaterial(130, 230, 0);
+    if (keyIsPressed === true) {
+      clear();
+    } 
 }
 
 function mouseClicked() {
    //clear();
    d = Math.sqrt((pmouseX - mouseX) * (pmouseX - mouseX) + (pmouseY - mouseY) * (pmouseY - mouseY))
    c = color(256*Math.random(),256*Math.random(),256*Math.random(),255 / (1 + d));
-   triangle(pmouseX, pmouseY, mouseX, mouseY, mouseX, mouseY);
+   //ellipse(innerWidth*Math.random(), innerHeight*Math.random(),innerWidth*Math.random(), innerHeight*Math.random());
+   textSize(75 * Math.random())
+   text('STOP',innerWidth*Math.random(), innerHeight*Math.random())
    fill(c);
 }
+
