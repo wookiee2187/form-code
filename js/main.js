4c13525122ce28e2e65fd73a0cc15829ae6ecf48
wookiee2187 <nlingareddy@uchicago.edu>
2020-06-08 00:04:54 -0400

    added comments and cleaned

--- js/main.js
+++ js/main.js
@@ -1,33 +1,47 @@
+/*
+I worked on an interactive piece where I wanted the
+piece to create something reminiscent of two doors (representing choice)
+when the mouse is dragged. When the dragging stops, and a click is
+registered, the word STOP appears somewhere. I wanted to comment on the
+difficulty of making a choice sometimes and how you might be overthinking
+rather than stopping and doing something.  I found using interactive art rather
+than generative art worked better for this message.
+
+Author - Neha Lingareddy
+Last updated - June 7th 2020
+Source - https://github.com/net-art-uchicago/form-code
+*/
+
+
+
 function setup() {
   createCanvas(innerWidth, innerHeight)
   background(255,255,255,100)
   let x = width / 2
   let y = height / 2
   let r = width / 4
-  //ellipse(x, y, r)
 }
 
 function mouseDragged() {
-    d = Math.sqrt((pmouseX - mouseX) * (pmouseX - mouseX) + (pmouseY - mouseY) * (pmouseY - mouseY))
-    c = color(256*Math.random(),256*Math.random(),256*Math.random(),255 / (1 + d));
-    //triangle(pmouseX, pmouseY, mouseX, mouseY, mouseX, mouseY);
-    //cylinder(mouseX,mouseY);
+    d = Math.sqrt((pmouseX - mouseX) * (pmouseX - mouseX) + (pmouseY - mouseY)
+     * (pmouseY - mouseY))
+    c = color(256*Math.random(),256*Math.random(),256*Math.random(),
+    255 / (1 + d));
+
     square(pmouseX, pmouseY, mouseX - mouseY);
-    //circle(pmouseX, pmouseY, mouseX - mouseY);
     fill(c);
-    //emissiveMaterial(130, 230, 0);
+
+    square(pmouseY, pmouseX, mouseY - mouseX);
+    fill(c);
     if (keyIsPressed === true) {
       clear();
-    } 
+    }
 }
 
 function mouseClicked() {
-   //clear();
    d = Math.sqrt((pmouseX - mouseX) * (pmouseX - mouseX) + (pmouseY - mouseY) * (pmouseY - mouseY))
    c = color(256*Math.random(),256*Math.random(),256*Math.random(),255 / (1 + d));
-   //ellipse(innerWidth*Math.random(), innerHeight*Math.random(),innerWidth*Math.random(), innerHeight*Math.random());
    textSize(75 * Math.random())
    text('STOP',innerWidth*Math.random(), innerHeight*Math.random())
    fill(c);
 }
-
